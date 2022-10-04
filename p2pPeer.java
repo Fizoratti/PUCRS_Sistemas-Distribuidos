ded48cd0d381613e216eb5fbeb2da4da8fe53f38
Robson Felipe Bittencourt <Robson_Bittencourt@Dellteam.com>
2022-10-04 11:03:12 -0300

    comments and increase timeout to 10s

--- p2pPeer.java
+++ p2pPeer.java
@@ -6,7 +6,7 @@ public class p2pPeer {
 
 	public static void main(String[] args) throws IOException {
 		if (args.length != 3) {
-			System.out.println("Uso: java p2pPeer <server> \"<message>\" <localport>");
+			System.out.println("Uso: java p2pPeer <server> \"<message>\" <localport>"); // localport é a porta que ele vai usar pra ele mesmo se comunicar com outros peers. quando roda o comando java p2pPeer 10.132.249.122 "create john" 9001  essa 9001 é a porta que o processo do novo peer vai ter.
 			System.out.println("<message> is:");
 			System.out.println("create nickname");
 			System.out.println("list nickname");
